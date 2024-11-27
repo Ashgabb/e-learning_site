40ecd21b853baa84ddb6fc4e5aea7477afba6e38
Ashgabb <niiamarhgabriel@gmail.com>
2024-11-27 00:22:44 +0000

    Project initiated

---
+++ components/Home/Navbar/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='fixed w-full transition-all duration-200 h-[12vh] z-[1000] bg-blue-700'>
+      Nav
+    </div>
+  )
+}
+
+export default Nav
---
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './Nav'
+import MobileNav from './MobileNav'
+
+const ResponsiveNav = () => {
+  return (
+    < >
+      <Nav />
+      <MobileNav />
+    </>
+
+  )
+}
+
+export default ResponsiveNav
